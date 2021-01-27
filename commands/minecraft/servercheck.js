cae55e46b9ce41085b8fbd0387f7848f32f9823c
Jackack <getjacked540c@gmail.com>
2021-01-27 21:45:16 +0000

    ip

--- commands/minecraft/servercheck.js
+++ commands/minecraft/servercheck.js
@@ -17,7 +17,7 @@ module.exports = {
                 port: ports,
             })
             .then((result) => {
-                check.edit(`\`\`\`nim\n--${args[0]}--\n\nServer Ip: ${result.srvRecord.host} \nVersion: ${result.version} \nOnline Players: ${result.onlinePlayers}  \nMax Players: ${result.maxPlayers}\`\`\``);
+                check.edit(`\`\`\`nim\n--${args[0]}--\n\nServer Ip: ${result.host} \nVersion: ${result.version} \nOnline Players: ${result.onlinePlayers}  \nMax Players: ${result.maxPlayers}\`\`\``);
             })
             .catch((err) => {
                 if (err) console.log(err);
