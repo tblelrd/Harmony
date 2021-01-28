6b48e9b1cceaeb858e061174be00441841b5a407
BuildTools <getjacked540c@gmail.com>
2021-01-28 19:28:39 +0000

    uptime

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ let connectedAmount = 0;
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end(connectedAmount.toString() + '\n' + (Math.floor((Date.now() - now) / 1000) / 60 / 60).toString() + ` hours since last launch (${Math.floor((Date.now() - now) / 1000) / 60}mins)`);
+  res.end(connectedAmount.toString() + '\n' + ((Math.floor(Date.now() - now) / 1000 / 60 / 60 / 100) * 100).toString() + ` hours since last launch (${Math.floor((Date.now() - now / 1000) / 60 / 100) * 100}mins)`);
 });
 
 server.on('connection', socket => {
