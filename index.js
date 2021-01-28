6a5b2d3d4d62690294daedddc1fcb3018245be4a
BuildTools <getjacked540c@gmail.com>
2021-01-28 13:00:15 +0000

    fix

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ let connectedAmount = 0;
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end(connectedAmount.toString() + now - Date.now());
+  res.end(connectedAmount.toString() + (now - Date.now()).toString());
 });
 
 server.on('connection', socket => {
