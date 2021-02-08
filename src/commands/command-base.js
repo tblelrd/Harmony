cc53fa9781c7e4653ecf198eec627ec311fed9e0
Jack <getjacked540c@gmail.com>
2021-02-08 19:43:04 +0000

    author bot

--- src/commands/command-base.js
+++ src/commands/command-base.js
@@ -75,7 +75,7 @@ module.exports = (client, commandOptions) => {
     }
     // Listen for messages
     client.on('message', msg => {
-        if(!msg.author.bot) return;
+        if(msg.author.bot) return;
         const {
             member,
             content,
