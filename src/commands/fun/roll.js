c4d981101b7f7f37e52ed6052e2c80cab9bf6cb0
Jack <getjacked540c@gmail.com>
2021-02-22 19:48:25 +0000

    roll command

---
+++ src/commands/fun/roll.js
@@ -0,0 +1,29 @@
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const dices = [];
+for(let i = 1; i <= 6; i++) {
+    fs.readFile(path.join(__dirname, `../../bin/dice/${i}.png`), (err, buffer) => {
+        if(err) return console.log(`${i} Failed`);
+
+        dices.push(buffer);
+    });
+}
+
+module.exports = {
+    commands: ['roll'],
+    maxArgs: 0,
+    callback: (msg) => {
+        const random = Math.floor(Math.random() * 6);
+
+        const dice = new MessageAttachment(dices[random], `${random.toString()}.png`);
+
+        const e = new MessageEmbed()
+        .setDescription(`**You rolled a ${random}!**`)
+        .attachFiles(dice)
+        .setImage(`attachment://${dice.name}`);
+
+        msg.channel.send(e);
+    },
+};
