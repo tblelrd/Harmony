19a22dff0c2fbd28d7cb10b7c941dee0cdc06d3b
Jack <getjacked540c@gmail.com>
2021-02-22 21:42:48 +0000

    invalid emoji

--- src/commands/utility/invite.js
+++ src/commands/utility/invite.js
@@ -1,10 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     commands: ['invite'],
     maxArgs: 0,
     dm: true,
-    callback: (msg) => {
+    callback: (msg, args, text, bot) => {
         try {
-            msg.channel.send('https://discord.com/api/oauth2/authorize?client_id=803556894487740447&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FAeXJxaE8&scope=bot');
+            const e = new MessageEmbed()
+            .setTitle('Link to invite me to your server')
+            .setURL('https://discord.com/api/oauth2/authorize?client_id=803556894487740447&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FAeXJxaE8&scope=bot')
+            .setAuthor(bot.user.username, bot.user.avatarURL());
+
+            msg.channel.send(e);
         } catch {
             msg.reply('err');
         }
