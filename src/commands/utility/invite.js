d693147a3d721eac097e13fca96d547437104ce5
Jack <getjacked540c@gmail.com>
2021-02-22 21:44:55 +0000

    fixed?

--- src/commands/utility/invite.js
+++ src/commands/utility/invite.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js');
+const { prefix } = require('../../../config.json'); 
 
 module.exports = {
     commands: ['invite'],
@@ -9,6 +10,7 @@ module.exports = {
             const e = new MessageEmbed()
             .setTitle('Link to invite me to your server')
             .setURL('https://discord.com/api/oauth2/authorize?client_id=803556894487740447&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FAeXJxaE8&scope=bot')
+            .setDescription(`\`${prefix}help\` to display help`)
             .setAuthor(bot.user.username, bot.user.avatarURL());
 
             msg.channel.send(e);
