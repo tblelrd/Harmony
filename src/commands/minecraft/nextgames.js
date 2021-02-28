44dd38f30677944ec7d55699f49146c1ccf39eed
Jackack <getjacked540c@gmail.com>
2021-02-28 17:26:49 +0000

    more bugs

--- src/commands/minecraft/nextgames.js
+++ src/commands/minecraft/nextgames.js
@@ -95,7 +95,7 @@ const bedwars = (oldStats, stats, amount, mode) => {
         kills_bedwars: o_kills_bedwars,
         deaths_bedwars: o_deaths_bedwars,
         final_kills_bedwars: o_final_kills_bedwars,
-        final_death_bedwars: o_final_deaths_bedwars,
+        final_deaths_bedwars: o_final_deaths_bedwars,
         beds_broken_bedwars: o_beds_broken_bedwars,
         beds_lost_bedwars: o_beds_lost_bedwars,
         losses_bedwars: o_losses_bedwars,
@@ -119,13 +119,13 @@ const bedwars = (oldStats, stats, amount, mode) => {
         kdr: Math.floor((kills_bedwars / (deaths_bedwars ? deaths_bedwars : 1)) * 100) / 100,
         fKills: final_kills_bedwars,
         fDeaths: final_deaths_bedwars,
-        fkdr: Math.floor((final_kills_bedwars, (final_deaths_bedwars ? final_deaths_bedwars : 1)) * 100) / 100,
+        fkdr: Math.floor((final_kills_bedwars / (final_deaths_bedwars ? final_deaths_bedwars : 1)) * 100) / 100,
         bBroke: beds_broken_bedwars,
         bLost: beds_lost_bedwars,
-        bblr: Math.floor((beds_broken_bedwars, (beds_lost_bedwars ? beds_lost_bedwars : 1)) * 100) / 100,
+        bblr: Math.floor((beds_broken_bedwars / (beds_lost_bedwars ? beds_lost_bedwars : 1)) * 100) / 100,
         losses: losses_bedwars,
         wins: wins_bedwars,
-        wlr: Math.floor((wins_bedwars, (losses_bedwars ? losses_bedwars : 1)) * 100) / 100,
+        wlr: Math.floor((wins_bedwars / (losses_bedwars ? losses_bedwars : 1)) * 100) / 100,
         played: games_played_bedwars,
 		amount: amount,
         mode: mode,
@@ -142,7 +142,6 @@ const skywars = (oldStats, stats, amount, mode) => {
         losses,
         games_played_skywars,
     } = stats;
-    console.log(deaths);
 
     const {
         deaths: o_deaths,
@@ -150,7 +149,6 @@ const skywars = (oldStats, stats, amount, mode) => {
         wins: o_wins,
         losses: o_losses,
     } = oldStats;
-    console.log(o_deaths);
 
     deaths -= o_deaths;
     kills -= o_kills;
@@ -160,10 +158,10 @@ const skywars = (oldStats, stats, amount, mode) => {
     const info = {
         kills: kills,
         deaths: deaths,
-        kdr: Math.floor((kills / deaths) * 100) / 100,
+        kdr: Math.floor((kills / (deaths ? deaths : 1)) * 100) / 100,
         wins: wins,
         losses: losses,
-        wlr: Math.floor((wins / losses) * 100) / 100,
+        wlr: Math.floor((wins / (losses ? losses : 1)) * 100) / 100,
         played: games_played_skywars,
         amount: amount,
         mode: mode,
@@ -203,7 +201,7 @@ const wait5games = async (info, mode, amount, msg) => {
                 },
             });
             const e = new MessageEmbed()
-            .setAuthor(msg.author.name, msg.author.avatarURL())
+            .setAuthor(msg.author.username, msg.author.avatarURL())
             .setTitle(`\`<${info.displayname}>\`'s stats for the past ${amount} games`)
             .addField('KDR', `${bedwarsStats.kills} / ${bedwarsStats.deaths}: ${bedwarsStats.kdr}`)
             .addField('FKDR', `${bedwarsStats.fKills} / ${bedwarsStats.fDeaths}: ${bedwarsStats.fkdr}`)
@@ -219,7 +217,7 @@ const wait5games = async (info, mode, amount, msg) => {
             },
         });
 		const e = new MessageEmbed()
-		.setAuthor(msg.author.name, msg.author.avatarURL())
+		.setAuthor(msg.author.username, msg.author.avatarURL())
         .setTitle(`\`<${info.displayname}>\`'s stats for the past ${amount} games`)
         .addField('KDR', `${bedwarsStats.kills} / ${bedwarsStats.deaths}: ${bedwarsStats.kdr}`)
         .addField('FKDR', `${bedwarsStats.fKills} / ${bedwarsStats.fDeaths}: ${bedwarsStats.fkdr}`)
