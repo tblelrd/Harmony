974825133451f654bb00ab89a38133be07e536da
Jack <getjacked540c@gmail.com>
2021-03-11 10:57:04 +0000

    Changes to dictionary command

--- src/commands/fun/dictionary.js
+++ src/commands/fun/dictionary.js
@@ -2,14 +2,15 @@ const { MessageEmbed } = require('discord.js');
 const urban = require('relevant-urban');
 
 module.exports = {
-    commands: ['dictionary', 'urban'],
+    commands: ['dictionary', 'urban', 'def', 'definition'],
     expectedArgs: '<word/s>',
     minArgs: 1,
     category: 'Fun',
     desc: 'Searches the word/s up on urban dictionary',
     callback: async (msg, args, text) => {
-        const def = await urban(text);
-        if(!def) return msg.channel.semd('Downs\'t exist!');
+        const def_array = await urban.all(text);
+        if(!def_array[0]) return msg.channel.semd('Downs\'t exist!');
+        const def = def_array[Math.floor(Math.random() * def_array.length)];
         const e = new MessageEmbed()
         .setAuthor(msg.author.username, msg.author.avatarURL())
         .setTitle(def.word)
