374809a28ac187167ffdb49f14245493832994e0
Jack <getjacked540c@gmail.com>
2021-04-19 20:39:50 +0100

    reddit comand

---
+++ src/commands/fun/reddit.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+const api = require('imageapi.js');
+
+module.exports = {
+    commands: ['reddit'],
+    expectedArgs: '<subreddit>',
+    minArgs: 1,
+    maxArgs: 1,
+    callback: async (msg, args) => {
+        try {
+            const reddit = await api.advanced(args[0]);
+            const e = new MessageEmbed()
+            .setTitle(reddit.title)
+            .setDescription(reddit.text)
+            .setAuthor(`u/${reddit.author.toLowerCase()}`, null, `https://reddit.com/user/${reddit.author.toLowerCase()}`)
+            .setColor('RANDOM')
+            .setImage(reddit.img);
+            msg.channel.send(e);
+        } catch {
+            msg.channel.send('Nope');
+        }
+    },
+};
